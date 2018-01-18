31225f548caf4ac798e9ee7c570d6b3516bbdb9b
ajzap1 <ajzap1@ymail.com>
2018-01-18 09:18:45 -0600

    New dot.c

--- dot.c
+++ dot.c
@@ -2,7 +2,7 @@ double dot(double a[], double b[], int length) {
 	int i;
 	double sum;
 	sum = 0;
-	for (i=0; i<= length; i++) { 
+	for (i=0; i < length; i++) { 
 		sum = sum + a[i]*b[i];
 		}
 	return sum;
